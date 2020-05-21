f1a5b1ddd3dc62e4f7c085bc9138de3f360bb470
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-21 23:36:31 +0530

    debug

--- plant_doc/utils/image_downloader.py
+++ plant_doc/utils/image_downloader.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 import requests
+import os
 
 logging.basicConfig(level=logging.NOTSET)
 logger = logging.getLogger("image_downloader")
@@ -9,6 +10,7 @@ def download_and_save(url):
     logger.debug(f"making request to server with url: {url}")
     resp = requests.get(url=url, verify=False)
     logger.debug("response received from server")
+    print(os.curdir)
     file_path = "./plant_doc/test_images/test"+str(url[url.rindex("."): len(url)])
     logger.debug("creating file to write response content")
     with open(file_path, "wb+") as f:
