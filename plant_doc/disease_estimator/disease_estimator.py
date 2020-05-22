59b40f9ab8c563f6da249f1508bd4c1721241f00
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-22 10:44:55 +0530

    disease_estimator

--- plant_doc/disease_estimator/disease_estimator.py
+++ plant_doc/disease_estimator/disease_estimator.py
@@ -1,3 +1,20 @@
 def diagnose(report, raw):
-    sorted_leaf_overall_color_contributions = sorted(report["leaf_overall_analysis_report"]["color_report"], key=lambda i: i['value'], reverse=True)
-    return(sorted_leaf_overall_color_contributions)
+    sorted_leaf_overall_color_contributions = sorted(report["leaf_overall_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
+    top_five_overall_colors = sorted_leaf_overall_color_contributions[0: 5]
+    
+    sorted_leaf_nerves_color_contributions = sorted(report["leaf_nerves_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
+    top_five_nerves_colors = sorted_leaf_nerves_color_contributions[0: 5]
+
+    sorted_leaf_branches_color_contributions = sorted(report["leaf_branch_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
+    top_five_branches_colors = sorted_leaf_branches_color_contributions[0: 5]
+
+    yellow_spots_count = report["leaf_texture_analysis_report"]["yellow"]["count"]
+    white_spots_count = report["leaf_texture_analysis_report"]["white"]["count"]
+    orange_spots_count = report["leaf_texture_analysis_report"]["orange"]["count"]
+
+    green_overall_rank = next((index for (index, d) in enumerate(sorted_leaf_overall_color_contributions) if d["name"] == "green"), None)
+    yellow_green_overall_rank = next((index for (index, d) in enumerate(sorted_leaf_overall_color_contributions) if d["name"] == "yellow_green"), None)
+    yellow_overall_rank = next((index for (index, d) in enumerate(sorted_leaf_overall_color_contributions) if d["name"] == "yellow"), None)
+    white_overall_rank = next((index for (index, d) in enumerate(sorted_leaf_overall_color_contributions) if d["name"] == "white"), None)
+
+    return white_overall_rank
