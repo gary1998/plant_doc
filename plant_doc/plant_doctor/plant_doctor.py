fcffdf03f39f55868cf45e4e8dfec23665ecc965
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-22 11:41:00 +0530

    disease_estimator

--- plant_doc/plant_doctor/plant_doctor.py
+++ plant_doc/plant_doctor/plant_doctor.py
@@ -35,7 +35,7 @@ def generate_report(url, size, mask_gray_low, mask_gray_high, health_point, spot
         leaf_branch_health = health_calculator.calculate(rgb_img, leaf_branch_points_img, health_point)
 
         # Leaf Spots Analysis Report
-        leaf_spots_report = leaf_spots.diagnose(rgb_img, mask, spot_area, spot_count)
+        leaf_spots_report = leaf_spots.diagnose(rgb_img, mask, spot_area)
 
         cache_remover.clean()
 
