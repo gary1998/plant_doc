27b5e8e9fa717d1f2ba26937ec2977b78c78b9b0
GAURAV GOSWAMI <gouravgoswami48@gmail.com>
2020-05-31 17:24:31 +0530

    report format alter

--- plant_doc/disease_estimator/disease_estimator.py
+++ plant_doc/disease_estimator/disease_estimator.py
@@ -1,16 +1,23 @@
 def diagnose(report, raw, spot_count):
-    sorted_leaf_overall_color_contributions = sorted(report["leaf_overall_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
+    sorted_leaf_overall_color_contributions = sorted(
+        report["leaf_overall_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
     top_five_overall_colors = sorted_leaf_overall_color_contributions[0: 5]
-    
-    sorted_leaf_nerves_color_contributions = sorted(report["leaf_nerves_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
+
+    sorted_leaf_nerves_color_contributions = sorted(
+        report["leaf_nerves_analysis_report"]["color_report"], key=lambda i: i["value"], reverse=True)
     top_five_nerves_colors = sorted_leaf_nerves_color_contributions[0: 5]
 
-    yellow_overall_rank = next((index for (index, d) in enumerate(top_five_overall_colors) if d["name"] == "yellow"), None)
-    purple_overall_rank = next((index for (index, d) in enumerate(top_five_overall_colors) if d["name"] == "purple"), None)
-    red_overall_rank = next((index for (index, d) in enumerate(top_five_overall_colors) if d["name"] == "red"), None)
-    yellow_green_overall_rank = next((index for (index, d) in enumerate(top_five_overall_colors) if d["name"] == "yellow_green"), None)
-    
-    yellow_nerve_rank = next((index for (index, d) in enumerate(top_five_nerves_colors) if d["name"] == "yellow"), None)
+    yellow_overall_rank = next((index for (index, d) in enumerate(
+        top_five_overall_colors) if d["name"] == "yellow"), None)
+    purple_overall_rank = next((index for (index, d) in enumerate(
+        top_five_overall_colors) if d["name"] == "purple"), None)
+    red_overall_rank = next((index for (index, d) in enumerate(
+        top_five_overall_colors) if d["name"] == "red"), None)
+    yellow_green_overall_rank = next((index for (index, d) in enumerate(
+        top_five_overall_colors) if d["name"] == "yellow_green"), None)
+
+    yellow_nerve_rank = next((index for (index, d) in enumerate(
+        top_five_nerves_colors) if d["name"] == "yellow"), None)
 
     yellow_spots = report["leaf_texture_analysis_report"]["yellow"]["presence"]
     brown_spots = report["leaf_texture_analysis_report"]["brown"]["presence"]
@@ -20,17 +27,23 @@ def diagnose(report, raw, spot_count):
     brown_spots_count = report["leaf_texture_analysis_report"]["brown"]["count"]
     red_spots_count = report["leaf_texture_analysis_report"]["red"]["count"]
 
-    burning = bool(yellow_overall_rank in range(3,4))
-    chlorosis = bool(yellow_overall_rank in range(1,2))
-    interveinal_chlorosis = bool(yellow_nerve_rank in range(1,2))
+    burning = bool(yellow_overall_rank in range(3, 4))
+    chlorosis = bool(yellow_overall_rank in range(1, 2))
+    interveinal_chlorosis = bool(yellow_nerve_rank in range(1, 2))
     mottling = bool(yellow_spots or brown_spots or red_spots)
-    necrosis = bool(brown_spots_count>=yellow_spots_count and brown_spots_count>=red_spots_count)
+    necrosis = bool(brown_spots_count >=
+                    yellow_spots_count and brown_spots_count >= red_spots_count)
     phosphorous_deficiency = bool(purple_overall_rank or red_overall_rank)
-    nitrogen_deficiency = bool((yellow_overall_rank or yellow_green_overall_rank) and not necrosis)
-    molybdenum_deficiency = bool((yellow_overall_rank or yellow_green_overall_rank) and necrosis)
-    magnesium_deficiency = bool(interveinal_chlorosis and red_spots_count>spot_count)
-    potassium_deficiency = bool((not interveinal_chlorosis) and chlorosis and brown_spots_count>spot_count)
-    chloride_deficiency = bool((not interveinal_chlorosis) and chlorosis and necrosis)
+    nitrogen_deficiency = bool(
+        (yellow_overall_rank or yellow_green_overall_rank) and not necrosis)
+    molybdenum_deficiency = bool(
+        (yellow_overall_rank or yellow_green_overall_rank) and necrosis)
+    magnesium_deficiency = bool(
+        interveinal_chlorosis and red_spots_count > spot_count)
+    potassium_deficiency = bool(
+        (not interveinal_chlorosis) and chlorosis and brown_spots_count > spot_count)
+    chloride_deficiency = bool(
+        (not interveinal_chlorosis) and chlorosis and necrosis)
 
     diagnosed_report = {
         "diseases": [],
@@ -39,102 +52,60 @@ def diagnose(report, raw, spot_count):
 
     if chlorosis:
         diagnosed_report["diseases"].append({
-            "chlorosis": {
-                "presence": chlorosis,
-                "text": "Chlorosis is generally caused due to lack of Iron in plants. Use some Iron based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "chlorosis",
+            "text": "Chlorosis is generally caused due to lack of Iron in plants. Use some Iron based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if burning:
         diagnosed_report["diseases"].append({
-            "burning": {
-                "presence": burning,
-                "text": "Burning is caused due to scorching sun light. Keep plants in shade for the time sun light is highest in day."
-            }
+            "name": "burning",
+            "text": "Burning is caused due to scorching sun light. Keep plants in shade for the time sun light is highest in day."
         })
     if interveinal_chlorosis:
         diagnosed_report["diseases"].append({
-            "interveinal_chlorosis": {
-                "presence": interveinal_chlorosis,
-                "text": "Interveinal Chlorosis is generally caused due to lack of Iron or Magnesium in plants. Use some Iron and Magnesium based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "interveinal_chlorosis",
+            "text": "Interveinal Chlorosis is generally caused due to lack of Iron or Magnesium in plants. Use some Iron and Magnesium based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if mottling:
         diagnosed_report["diseases"].append({
-            "mottling": {
-                "presence": mottling,
-                "text": "Mottling in leaves can be sign of bacterial or fungal infection on smaller portion of crop or disease on whole crop. If Mottling is seen on almost all leaves, use Calcium and Magnesium based fertilizer or use some fungicide to spray over bacteria or fungus infected portions of crop. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "mottling",
+            "text": "Mottling in leaves can be sign of bacterial or fungal infection on smaller portion of crop or disease on whole crop. If Mottling is seen on almost all leaves, use Calcium and Magnesium based fertilizer or use some fungicide to spray over bacteria or fungus infected portions of crop. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if necrosis:
         diagnosed_report["diseases"].append({
-            "necrosis": {
-                "presence": necrosis,
-                "text": "Necrosis in leaves can be sign of bacterial or fungal infection on smaller portion of crop or disease on whole crop. If Mottling is seen on almost all leaves, use Phosphorous based fertilizer or use some fungicide to spray over bacteria or fungus infected portions of crop. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "necrosis",
+            "text": "Necrosis in leaves can be sign of bacterial or fungal infection on smaller portion of crop or disease on whole crop. If Mottling is seen on almost all leaves, use Phosphorous based fertilizer or use some fungicide to spray over bacteria or fungus infected portions of crop. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if phosphorous_deficiency:
         diagnosed_report["deficiencies"].append({
-            "phosphorous_deficiency": {
-                "presence": phosphorous_deficiency,
-                "text": "Use Phosphorous based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "phosphorous_deficiency",
+            "text": "Use Phosphorous based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if nitrogen_deficiency:
         diagnosed_report["deficiencies"].append({
-            "nitrogen_deficiency": {
-                "presence": nitrogen_deficiency,
-                "text": "Use Nitrogen based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "nitrogen_deficiency",
+            "text": "Use Nitrogen based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if molybdenum_deficiency:
         diagnosed_report["deficiencies"].append({
-            "molybdenum_deficiency": {
-                "presence": molybdenum_deficiency,
-                "text": "Use Molybdenum based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "molybdenum_deficiency",
+            "text": "Use Molybdenum based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if magnesium_deficiency:
         diagnosed_report["deficiencies"].append({
-            "magnesium_deficiency": {
-                "presence": magnesium_deficiency,
-                "text": "Use Magnesium based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "magnesium_deficiency",
+            "text": "Use Magnesium based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if potassium_deficiency:
         diagnosed_report["deficiencies"].append({
-            "potassium_deficiency": {
-                "presence": potassium_deficiency,
-                "text": "Use Potassium based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": "potassium_deficiency",
+            "text": "Use Potassium based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if chloride_deficiency:
         diagnosed_report["deficiencies"].append({
-            "chloride_deficiency": {
-                "presence": chloride_deficiency,
-                "text": "Use Chloride based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
-            }
+            "name": chloride_deficiency,
+            "text": "Use Chloride based fertilizer. Fertilizer's amount should be fixed according to field area and yield amount."
         })
     if raw:
         diagnosed_report["raw"] = report
 
     return diagnosed_report
-
-    # return {
-    #     "burning": burning,
-    #     "chlorosis": {
-    #         "presence": chlorosis,
-    #         "cure": {
-    #             "Mix chelated iron powder in water and supply to soil. Make sure to use amount of chelated iron according to field area and yield amount."
-    #         }
-    #     },
-    #     "interveinal_chlorosis": interveinal_chlorosis,
-    #     "mottling": mottling,
-    #     "necrosis": necrosis,
-    #     "phosphorous_deficiency": phosphorous_deficiency,
-    #     "nitrogen_deficiency": nitrogen_deficiency,
-    #     "molybdenum_deficiency": molybdenum_deficiency,
-    #     "magnesium_deficiency": magnesium_deficiency,
-    #     "potassium_deficiency": potassium_deficiency,
-    #     "chloride_deficiency": chloride_deficiency,
-    #     "raw_report": raw_report
-    # }
